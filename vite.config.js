ce63bdf9b136c6f60b6b84b12a8388d94dd1c0b6
joseaugustonunez <joseaugustonunezvicente@gmail.com>
2025-11-22 11:50:49 -0500

    sin vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // O la ruta base de tu aplicación si no está en la raíz
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
